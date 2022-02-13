c8ac53c007c1c690a53e8a932e75117331e39204
lm93547 <lucasmartin045@gmail.com>
2022-02-13 20:39:34 +0000

    adding header

---
+++ app/components/Header/index.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * Header
+ *
+ */
+
+import React, { memo } from 'react';
+// import PropTypes from 'prop-types';
+import './style.css'
+
+function Header() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+Header.propTypes = {};
+
+export default memo(Header);
