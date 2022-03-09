abaf10c4a93833b61f424a260a088f7025cc0867
lm93547 <lucasmartin045@gmail.com>
2022-03-09 11:17:46 +0000

    update test for actions

--- cypress/integration/MapPage.spec.ts
+++ cypress/integration/MapPage.spec.ts
@@ -1,7 +1,7 @@
 describe('Test for MapPage', () => {
   it('Visits site and see if the MapPage renders', () => {
     cy.visit('http://localhost:3000/');
-    cy.viewport(1920, 1080);
+    cy.viewport(1280, 720);
     // snapshot name will be the test title
     cy.wait(2000);
     cy.matchImageSnapshot();
